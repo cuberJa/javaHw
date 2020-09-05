fd1fa66b014825a217ab68446f1768a2ab33024b
constr <constr85@mail.ru>
2020-09-05 18:05:32 +0300

    HW04-collections
    + fixed module names in settings file

---
+++ L04_collections/src/test/java/com/pzin/MyMapIntTest.java
@@ -0,0 +1,34 @@
+package com.pzin;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class MyMapIntTest {
+
+    @Test
+    void putAndGet() {
+        MyMapInt map = new MyMapInt(1);
+        int value = 88;
+        String key = "key";
+
+        map.put(key, value);
+        assertEquals(value, map.get(key));
+    }
+
+    @Test
+    void putAndGetSequence() {
+        int size = 10;
+        String keyStr = "k";
+        MyMapInt map = new MyMapInt(size);
+
+        for (int idx = 0; idx < size; idx++) {
+            map.put(keyStr + idx, idx);
+        }
+
+        for (int idx = 0; idx < size; idx++) {
+            assertEquals(idx, map.get(keyStr + idx));
+        }
+    }
+
+}
