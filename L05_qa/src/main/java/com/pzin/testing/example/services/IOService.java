5496df82041fd983d6bcf81dadafca8d48f8a8b2
constr <constr85@mail.ru>
2020-09-10 08:25:52 +0300

    HW05-quality assurance

---
+++ L05_qa/src/main/java/com/pzin/testing/example/services/IOService.java
@@ -0,0 +1,7 @@
+package com.pzin.testing.example.services;
+
+public interface IOService {
+    void out(String message);
+
+    String readString();
+}
---
+++ L05_qa/src/main/java/com/pzin/testing/exercise/MessageTemplateProvider.java
@@ -0,0 +1,5 @@
+package com.pzin.testing.exercise;
+
+public interface MessageTemplateProvider {
+    String getMessageTemplate(String templateName);
+}
