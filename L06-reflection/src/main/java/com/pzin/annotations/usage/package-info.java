fed67406f4dfb66126cf52554603c2d9906ebf92
constr <constr85@mail.ru>
2020-09-11 21:33:43 +0300

    HW05-quality assurance

---
+++ L06-reflection/src/main/java/com/pzin/annotations/usage/package-info.java
@@ -0,0 +1,4 @@
+@PackageOwner(owner = "tully") //ElementType.PACKAGE
+        package com.pzin.annotations.usage;
+
+import com.pzin.annotations.PackageOwner;
---
+++ L06-reflection/src/main/java/com/pzin/reflection/SimpleAnnotation.java
@@ -0,0 +1,8 @@
+package com.pzin.reflection;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SimpleAnnotation {
+}
